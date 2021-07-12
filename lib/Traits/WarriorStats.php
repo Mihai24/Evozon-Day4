0bc27c4db234909ce60819226a6bbdb6c3a81c41
Mihai24 <56975056+Mihai24@users.noreply.github.com>
2021-07-12 12:03:51 +0300

    Small update
    - added WarriorStats as trait
    - added ChildrenOfIluvatar

---
+++ lib/Traits/WarriorStats.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BattleOfMoria\Traits;
+
+trait WarriorStats
+{
+    private float $strength;
+
+    private float $intelligence;
+
+    private float $charisma;
+
+    private float $fightPower;
+}
